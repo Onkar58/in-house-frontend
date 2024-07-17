387a7a02b557e5d03608978463bc65d8785010c9
Onkar Waghmode <onkar58@Onkars-MacBook-Air.local>
2024-07-17 08:48:36 +0530

    refactor: added the loading message

--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -6,48 +6,13 @@ const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
   useEffect(() => {
     onAuthStateChanged(auth, () => {
       setUser(auth.currentUser);
     });
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
   }, []);
   return (
-    <UserContext.Provider value={{ user }}>
-      {loading ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-40"
-          viewBox="0 0 200 200"
-        >
-          <circle
-            fill="none"
-            stroke="white"
-            strokeWidth="15"
-            strokeLinecap="round"
-            strokeDasharray="0 44 0 44 0 44 0 44 0 360"
-            cx="100"
-            cy="100"
-            r="70"
-            transform-origin="center"
-          >
-            <animateTransform
-              type="rotate"
-              attributeName="transform"
-              calcMode="discrete"
-              dur="2"
-              values="360;324;288;252;216;180;144;108;72;36"
-              repeatCount="indefinite"
-            ></animateTransform>
-          </circle>
-        </svg>
-      ) : (
-        children
-      )}
-    </UserContext.Provider>
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
   );
 };
 
